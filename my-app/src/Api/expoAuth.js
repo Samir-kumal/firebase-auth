dfc841d1a14383a7eb99246fb682809f70d474d6
Samir-kumal <kumalsameer124@gmail.com>
2023-06-20 12:48:34 +0545

    changes saved

--- my-app/src/Api/expoAuth.js
+++ my-app/src/Api/expoAuth.js
@@ -1,46 +1,34 @@
 import React from "react";
-
 import { Text, Image } from "react-native";
 import SignUpTemplate from "../components/common/CardTemplate";
 import { images } from "../constants";
-// import { useAuth } from "../../src/context/Auth";
-import * as Google from "expo-auth-session/providers/google";
-import * as WebBrowser from "expo-web-browser";
-import { promptAsync } from "expo-auth-session/providers/google";
-import {
-  GoogleAuthProvider,
-  onAuthStateChanged,
-  signInWithCredential,
-} from "firebase/auth";
-
-import { auth } from "../../firebaseConfig";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-WebBrowser.maybeCompleteAuthSession();
+import { useAuth } from "../../src/context/Auth";
 
 function GoogleAuth(props) {
-  const [userInfo, setUserInfo] = React.useState();
-  // const [request, response, promptAsync] = Google.useAuthRequest({
-  //   iosClientId:
-  //     "833484929166-up0em00kop7t255kjpaq6on08esce8q2.apps.googleusercontent.com",
-  //   androidClientId:
-  //     "833484929166-2h4893uhbic1i11jambtbbbvjoiihppd.apps.googleusercontent.com",
-  //   expoClientId:
-  //     "833484929166-une407snklcutr7r8sam2osdlf9jqne7.apps.googleusercontent.com",
-  // });
+  const { signInWithGoogle } = useAuth();
 
-  // React.useEffect(() => {
-  //   if (response?.type === "success") {
-  //     const { id_token } = response.params;
-  //     const credential = GoogleAuthProvider.credential(id_token);
-  //     signInWithCredential(auth, credential);
-  //   }
-  // }, [response]);
+  const handleGoogleSignIn = async () => {
+    try {
+      await props.promptAsync();
+
+      // Perform the necessary logic to trigger the Google sign-in flow
+      // For example, you can call the `promptAsync` function if using Expo Google authentication
+      // const result = await promptAsync();
+      // if (result.type === "success") {
+      //   const { id_token } = result.params;
+      //   signInWithGoogle(id_token);
+      // }
+    } catch (error) {
+      // Handle any errors that occur during the sign-in process
+    }
+  };
 
   return (
-    <SignUpTemplate onPress={props.promptAsync}>
-      <Image className="h-6 w-6" source={images.google} />
-      <Text className="text-xl">Google</Text>
+    <SignUpTemplate onPress={handleGoogleSignIn}>
+      <Image style={{ height: 24, width: 24 }} source={images.google} />
+      <Text style={{ fontSize: 20 }}>Google</Text>
     </SignUpTemplate>
   );
 }
+
 export default GoogleAuth;
